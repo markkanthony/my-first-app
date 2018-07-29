41cbf60faaf2aba34467d1caf19cc94bf5471176
markkanthony <mmaano1996@gmail.com>
2018-07-29 18:39:16 +0800

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'main-src',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Hello wordl';
 }
+ 
